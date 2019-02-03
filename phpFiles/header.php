bb0d6e3b9fe5ad9c69ace05339dc43fe373ee145
TakuyaTomioka <taku.netbusiness@gmail.com>
2019-02-04 08:12:40 +0900

    Create loginsystem from header.php

---
+++ phpFiles/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#">
+                <img src="" alt="">
+            </a>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">Portfolio</a></li>
+                <li><a href="">About me</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div>
+                <form action="includes/login.inc.php" method="post">
+                    <input type="text" name="mailuid" placeholder="Username/Email...">
+                    <input type="password" name="pwd" placeholder="Password...">
+                    <button type="submit" name="login-submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>
+                <form action="includes/logout.inc.php" method="post">
+                    <button type="submit" name="logout-submit">Logout</button>
+                </form>
+            </div>
+        </nav>
+    
+    </header>
